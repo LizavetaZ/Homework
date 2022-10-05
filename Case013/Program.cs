5d77aa8c34c141a0a79207c0b64c21c4d0bb1216
Лиза за <lizuska@yandex.ru>
2022-10-05 22:50:50 +0300

    remark

--- Case013/Program.cs
+++ Case013/Program.cs
@@ -2,12 +2,20 @@
 string value = Console.ReadLine();
 int a = int.Parse(value);
 int b = a/100;
-if (b<1)
+if (Math.Abs(b) < 1)
 {
     Console.Write ("третьей цифры нет");
 }
 else
 {
-    string c = a.ToString();
-    Console.Write ("третья цифра " + c[2]);
+    if (a>0)
+    {
+        string c = a.ToString();
+        Console.Write ("третья цифра " + c[2]);
+    }
+    else 
+    {
+        string c = a.ToString();
+        Console.Write ("третья цифра " + c[3]);
+    }
 }
