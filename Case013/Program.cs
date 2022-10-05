9274985c6767d26a1695cffa3cb7021fbaa3ec42
Лиза за <lizuska@yandex.ru>
2022-10-05 21:02:44 +0300

    third digit

---
+++ Case013/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine ("Введите число");
+string value = Console.ReadLine();
+int a = int.Parse(value);
+int b = a/100;
+if (b<1)
+{
+    Console.Write ("третьей цифры нет");
+}
+else
+{
+    string c = a.ToString();
+    Console.Write ("третья цифра " + c[2]);
+}
