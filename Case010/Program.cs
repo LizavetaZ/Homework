cee45797d4d5e1eea219388a0592ef813ea078cb
Лиза за <lizuska@yandex.ru>
2022-10-05 20:21:53 +0300

    second digit

---
+++ Case010/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Введите число");
+string value = Console.ReadLine();
+int a = int.Parse(value);
+int c = a%100;
+int d = c/10;
+Console.Write ("Вторая цифра трехзначного числа " + d);
+
+//второй вариант
+//Console.WriteLine ("Введите число");
+//string value = Console.ReadLine().ToString();
+//Console.Write ("Вторая цифра трехзначного числа " + value[1]);
