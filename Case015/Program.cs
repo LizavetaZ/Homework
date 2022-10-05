4582ff31afaa1e1a86c50f70505aa9736c09b1d2
Лиза за <lizuska@yandex.ru>
2022-10-05 21:04:05 +0300

    weekends

---
+++ Case015/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Введите число");
+string value = Console.ReadLine();
+int a = int.Parse(value);
+if (a==6 || a==7)
+{
+    Console.Write ("да");
+}
+else 
+{
+    Console.Write ("нет");
+}
