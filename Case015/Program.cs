8955026cb2021b49f659193ad07c387ddab4307b
Лиза за <lizuska@yandex.ru>
2022-10-06 17:32:23 +0300

    remark

--- Case015/Program.cs
+++ Case015/Program.cs
@@ -3,9 +3,13 @@ string value = Console.ReadLine();
 int a = int.Parse(value);
 if (a==6 || a==7)
 {
-    Console.Write ("да");
+    Console.Write ("выходной");
+}
+else if (a>0 && a<6)
+{
+    Console.Write ("будний");
 }
 else 
 {
-    Console.Write ("нет");
+    Console.Write ("ошибка");
 }
